a7365cb20df1a2b78c8fee065ef6110b4bfd9234
Kap-pax <arkadiusz.kapa.mail@gmail.com>
2025-05-11 23:52:13 +0200

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.Win32;
+using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace StudentApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+        private void New_Click(object sender, RoutedEventArgs e)
+        {
+            Window1 AddStudent = new Window1();
+            AddStudent.ShowDialog();
+        }
+        private void Open_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Pliki CSV z separatorem (,) |*.csv|Pliki CSV z separatorem (;) |*.csv";
+            openFileDialog.Title = "Otwórz plik CSV";
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                mainList.Items.Clear();
+                string delimiter = openFileDialog.FilterIndex == 1 ? "," : ";";
+                var lines = File.ReadAllLines(openFileDialog.FileName, Encoding.UTF8);
+
+                foreach (var line in lines)
+                {
+                    string[] parts = line.Split(delimiter);
+                    Person Student = new()
+                    {
+                        m_PESEL = parts.ElementAtOrDefault(0),
+                        m_Name = parts.ElementAtOrDefault(1),
+                        m_SecName = parts.ElementAtOrDefault(2),
+                        m_Surname = parts.ElementAtOrDefault(3)
+                    };
+                    mainList.Items.Add(Student);
+                }
+            }
+        }
+
+
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+
+            saveFileDialog.Filter = "Pliki CSV z separatorem (,) |*.csv|Pliki CSV z separatorem (;) |*.csv";
+            saveFileDialog.Title = "Zapisz jako plik CSV";
+
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                string filePath = saveFileDialog.FileName;
+                string delimiter = ";";
+                if (saveFileDialog.FilterIndex == 1)
+                {
+                    delimiter = ",";
+                }
+
+                using (StreamWriter writer = new StreamWriter(filePath))
+                {
+                    foreach (Person item in mainList.Items)
+                    {
+                        var row = $"{item.m_PESEL}{delimiter}{item.m_Name}" +
+                            $"{delimiter}{item.m_SecName}{delimiter}{item.m_Surname}";
+                        writer.WriteLine(row);
+                    }
+                }
+            }
+        }
+        public class Person
+        {
+            public string? m_PESEL { get; set; }
+            public string? m_Name { get; set; }
+            public string? m_SecName { get; set; }
+            public string? m_Surname { get; set; }
+            public string? m_DateOfBirth { get; set; }
+            public string? m_PhoneNumber { get; set; }
+            public string? m_Location { get; set; }
+            public string? m_City { get; set; }
+            public string? m_Code { get; set; }
+        }
+    }
+}
---
+++ Window1.xaml.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace StudentApp
+{
+    /// <summary>
+    /// Logika interakcji dla klasy Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+        private bool ValidateInput()
+        {
+            if (string.IsNullOrEmpty(txtPesel.Text))
+            {
+                txtPesel.Background = Brushes.Red;
+                return false;
+                
+            }
+            else if (!(ValidatePesel(txtPesel.Text)))
+            {
+                return false;
+            }
+
+
+
+
+             if(string.IsNullOrEmpty(txtName.Text) && !(int.TryParse(txtName.Text, out _)))
+            {
+                txtName.Background = Brushes.Red;
+                return false;
+                
+            }
+
+
+             if (string.IsNullOrEmpty(txtSurname.Text) && !(int.TryParse(txtSurname.Text, out _)))
+            {
+                txtSurname.Background = Brushes.Red;
+                return false;
+
+            }
+
+
+
+             if (string.IsNullOrEmpty(txtDateOfBirth.Text))
+            {
+                txtDateOfBirth.Background = Brushes.Red;
+                return false;
+
+            }
+
+
+
+             if (string.IsNullOrEmpty(txtLocation.Text))
+            {
+                txtLocation.Background = Brushes.Red;
+                return false;
+
+            }
+
+
+
+             if (string.IsNullOrEmpty(txtCity.Text))
+            {
+                txtCity.Background = Brushes.Red;
+                return false;
+
+            }
+
+
+
+            if (string.IsNullOrEmpty(txtCode.Text))
+            {
+                txtCode.Background = Brushes.Red;
+                return false;
+
+            }
+
+
+
+            return true;
+        }
+        private bool ValidatePesel(string pesel)
+        {
+            int[] w = { 1, 3, 7, 9, 1, 3, 7, 9, 1, 3 };
+            int s = 0;
+            if (pesel.Length == 11) 
+            {
+                if (int.TryParse(pesel , out _))
+                {
+                    for (int i = 0; i < 10; i++)
+                    {
+                        s += pesel[i] * w[i];
+                    }
+                    int o1 = s % 10;
+                    int o2 = 10 - o1;
+                    o1 = o2 % 10;
+                    if (o1 == pesel[-1])
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+            return false;
+        }
+    }
+}
